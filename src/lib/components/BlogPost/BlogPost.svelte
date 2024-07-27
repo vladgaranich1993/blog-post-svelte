e6c02b4dc102be677ea5eb2770080ab1720c5489
Vlad Haranich <v.haranich@palfinger.com>
2024-07-27 16:45:34 +0200

    create BlogPost component, moving logic into component

---
+++ src/lib/components/BlogPost/BlogPost.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  export let title: string;
+  export let content: string;
+  export let postUser: string;
+  export let postTime: any;
+  export let postId: string;
+  import userStore from '$lib/userStore';
+  import type { User } from 'firebase/auth';
+
+  import { createEventDispatcher, onMount } from 'svelte';
+  const dispatch = createEventDispatcher();
+
+  let user: User | null = null;
+  $: user = $userStore;
+
+  function handleDelete() {
+    dispatch('deletePost', postId);
+  }
+</script>
+
+<div class="w-full bg-white rounded shadow-lg p-8 m-4 md:max-w-md md:mx-auto">
+  <h2>{title}</h2>
+  <p>{content}</p>
+  <p>
+    <small>By {postUser} on {new Date(postTime.toDate()).toLocaleString()}</small>
+  </p>
+  {#if user && user.email === postUser}
+    <button
+      class="mt-2 bg-red-500 hover:bg-red-700 text-white uppercase text-sm font-semibold px-4 py-2 rounded"
+      on:click={() => handleDelete()}>
+        Delete
+    </button>
+  {/if}
+</div>
